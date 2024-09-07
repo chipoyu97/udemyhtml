dfaf7538f7f13e6fca07957c0888e10fea49dfc5
chipoyu <chipoyu97@gmail.com>
2024-09-08 00:08:41 +0800

    add

--- 20.0 The Simon Game/Simon Game Challenge Starting Files/game.js
+++ 20.0 The Simon Game/Simon Game Challenge Starting Files/game.js
@@ -1,17 +1,57 @@
 var buttonColours = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
+var userClickedPattern = [];
+var level = 0;
+//You'll need a way to keep track of whether if the game has started or not, so you only call nextSequence() on the first keypress.
+var started = false;
+var mouseClick = 0;
 
+$(".btn").click(function(){
+    
+    var userChosenColour = $(this).attr("id");
+    userClickedPattern.push(userChosenColour);
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
+    mouseClick++;
+    //console.log(mouseClick);
+    checkAnswer(mouseClick);
+})
 
 function nextSequence(){
+    level += 1;
+    $("#level-title").text("Level " + level);
     var randomNumber = Math.floor(Math.random()*4);
     var randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
     result = "#" + randomChosenColour;
-    console.log(result);
-    $(result).click(function(){
-        $(result).fadeIn(100).fadeOut(100).fadeIn(100);
-        audio_path = ("sounds/" + randomChosenColour + ".mp3");
-        new Audio(audio_path).play();
-    })
+    playSound(randomChosenColour);
 }
 
+
+function playSound(name){
+    var audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+    
+}
+
+function animatePress(currentColour){
+    var currentColour = currentColour;
+    console.log("#"+currentColour);
+    $("#"+currentColour).addClass("pressed");
+    setTimeout(function() {
+        $("#"+currentColour).removeClass("pressed");
+      }, 100);
+}
+
+
+$(document).on("keydown", function(event){
+    if(event.key == "a" && !started) { 
+        nextSequence();
+        started = True;
+    }
+})
+
+function checkAnswer(currentLevel){
+    console.log(currentLevel);
+    
+}
